69b0d921b6282f9ab9a0a62a9d9f5f1d354a7fb3
Polymershot <69413289+Polymershot@users.noreply.github.com>
2020-10-11 19:06:14 -0700

    Update Nxn Matrix Solver v2.py

--- v1. + v2./Nxn Matrix Solver v2.py
+++ v1. + v2./Nxn Matrix Solver v2.py
@@ -4,28 +4,27 @@ from itertools import repeat
 
 def nxn_matrix_solver(leftarr,rightarr):
     # initialize arrays
-    left = np.array(leftarr)
-    rightarray = np.array(rightarr)
-
-    lenofcol = np.shape(left)[0]
-
+    leftarr = np.array(leftarr)
+    rightarr = np.array(rightarr)
+    
     # initialize list to place determinants in
-    variable = list(repeat(0, lenofcol))
-
-    right = rightarray.T
-    leftcopy = left.copy()
-    determinant_arr =  np.linalg.det(left)
+    variables = list(repeat(0, lenofcol))
+    
+    lenofcol = np.shape(leftarr)[0]
+    vector_col = rightarr.T
+    leftcopy = leftarr.copy()
+    determinant_arr =  np.linalg.det(leftarr)
 
     # finding dx,dy,dz...
-    for i in range((np.shape(left)[0])):
+    for i in range((np.shape(leftarr)[0])):
         temp = leftcopy[:,i]
-        left[:,i] = right[:,0]
-        variable[i] += np.linalg.det(left)
-        left[:,i] = temp
+        leftarr[:,i] = right_vector[:,0]
+        variables[i] += np.linalg.det(leftarr)
+        leftarr[:,i] = temp
 
     #printing results
-    for i in range(len(variable)):
-        variable_answer = round(variable[i]/determinant_arr)
+    for i in range(len(variables)):
+        variable_answer = round(variables[i]/determinant_arr)
         print('Variable ' + str(i + 1) + ' = ' + str(variable_answer))
 
 nxn_matrix_solver([[1,2,1,-1],[3,2,4,4],[4,4,3,4],[2,0,1,5]],[[5,16,22,15]])
